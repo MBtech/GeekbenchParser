9371daef24e02192d8a722ebbe4a9bcfc5626523
MB <mbilal_ce@live.com>
2016-07-06 15:29:11 +0200

    write to CSV file

--- src/main/java/com/learningcurve/mb/geekbenchparser/GeekbenchParser.java
+++ src/main/java/com/learningcurve/mb/geekbenchparser/GeekbenchParser.java
@@ -5,9 +5,16 @@
  */
 package com.learningcurve.mb.geekbenchparser;
 
+import au.com.bytecode.opencsv.CSVWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -15,12 +22,12 @@ import org.jsoup.select.Elements;
 
 /**
  * Program to return the numbers from Geekbench 3 run for programmatic use.
- * Args: takes the URL that is provided at the end geekbench run 
- * Returns: a HashMap containing 3 entries. Each entry is a HashMap with several subsequent entries
- * containing results for the benchmarks in each category
- * Entry 1: Integer Performance
- * Entry 2: Floating Point Performance
- * Entry 3: Memory Performance
+ * Args: takes the URL that is provided at the end geekbench run Returns: a
+ * HashMap containing 3 entries. Each entry is a HashMap with several subsequent
+ * entries containing results for the benchmarks in each category Entry 1:
+ * Integer Performance Entry 2: Floating Point Performance Entry 3: Memory
+ * Performance
+ *
  * @author Muhammad Bilal
  */
 public class GeekbenchParser {
@@ -29,20 +36,38 @@ public class GeekbenchParser {
         System.out.println(args[0]);
         String url = args[0];
         Document doc = Jsoup.connect(url).get();
-        System.out.println(parseBenchmarkInfo(doc));
+        HashMap<String, HashMap<String, String>> results = parseBenchmarkInfo(doc);
+
+        for (String cat : results.keySet()) {
+            File file = new File(cat + "_results.csv");
+            boolean headerFlag = !file.exists();
+            CSVWriter writer = new CSVWriter(new FileWriter(cat + "_results.csv",true));
+            Set<String> keys = results.get(cat).keySet();
+            System.out.println(keys);
+            String[] header = keys.toArray(new String[keys.size()]);
+            if (headerFlag) {
+                writer.writeNext(header);
+            }
+            ArrayList<String> vals = new ArrayList<>();
+            for (int i = 0; i < header.length; i++) {
+                vals.add(results.get(cat).get(header[i]));
+            }
+            String[] values = vals.toArray(new String[vals.size()]);
+            writer.writeNext(values);
+            writer.close();
+        }
 
     }
 
-    public static HashMap<String, HashMap<String,String>> parseBenchmarkInfo(Document doc) {
-        HashMap<String, HashMap<String,String>> result = new HashMap<>();
-        String [] perfBenchmark = {"Integer Performance"
-                ,"Floating Point Performance","Memory Performance"};
+    public static HashMap<String, HashMap<String, String>> parseBenchmarkInfo(Document doc) {
+        HashMap<String, HashMap<String, String>> result = new HashMap<>();
+        String[] perfBenchmark = {"Integer Performance", "Floating Point Performance", "Memory Performance"};
 //        Elements elements = doc.select("div.span9").select("table");
 //        for (Element e : elements) {
 //            System.out.println(e.select("tbody").text());
 //            //System.out.println(e.text());
 //        }
-        
+
         Elements benchmarks = doc.select("div.span9").select("table.table.section-performance");
 
         for (int i = 0; i < 3; i++) {
@@ -52,9 +77,9 @@ public class GeekbenchParser {
                     .select("tr").select("td.name");
             Iterator<Element> score = perf.iterator();
             Iterator<Element> title = perfTitle.iterator();
-            HashMap<String,String> temp = new HashMap<>();
+            HashMap<String, String> temp = new HashMap<>();
             while (title.hasNext() && score.hasNext()) {
-                String num = score.next().text().split(" ")[1].concat(" "+ score.next().text().split(" ")[2]);
+                String num = score.next().text().split(" ")[1].concat(" " + score.next().text().split(" ")[2]);
                 String name = title.next().text();
                 temp.put(name, num);
                 //System.out.println(name + " : " + num);
